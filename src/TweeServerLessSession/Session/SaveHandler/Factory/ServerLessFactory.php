b24c72a4d65481521af6907b01becac86a669b73
Rostislav Mykhajliw <necromant2005@gmail.com>
2016-05-20 22:35:23 +0300

    update factory

--- src/TweeServerLessSession/Session/SaveHandler/Factory/ServerLessFactory.php
+++ src/TweeServerLessSession/Session/SaveHandler/Factory/ServerLessFactory.php
@@ -1,14 +1,14 @@
 <?php
 namespace TweeServerLessSession\Session\SaveHandler\Factory;
 
-use TweeServerLessSession\Session\SaveHandler\StorageLess;
+use TweeServerLessSession\Session\SaveHandler\ServerLess;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class ServerLessFactory
 {
     public function __invoke(ServiceLocatorInterface $container)
     {
-        return new StorageLess(
+        return new ServerLess(
             $container->get('request'),
             $container->get('response'),
             $container->get('TweeServerLessSessionn\Session\Crypter')
