faec8b5173f6722c96c88ff624a2d83e55e4d9d8
Rostislav Mykhajliw <necromant2005@gmail.com>
2016-05-20 23:13:03 +0300

    use crypter key

--- src/TweeServerLessSession/Session/Crypter.php
+++ src/TweeServerLessSession/Session/Crypter.php
@@ -3,13 +3,92 @@ namespace TweeServerLessSession\Session;
 
 class Crypter implements CrypterInterface
 {
-    public function encrypt($data)
+    protected $key = '';
+
+    public function __construct($key)
     {
-        return base64_decode($data);
+        $this->setKey($key);
     }
 
-    public function decrypt($crypted)
+    public function setKey($key)
     {
+        $this->key = $key;
+    }
+
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    public function encrypt($data)
+    {
+        $iv = openssl_random_pseudo_bytes(16); // AES block size in CBC mode
+
+        // Encryption
+        $ciphertext = openssl_encrypt(
+            $data,
+            'AES-256-CBC',
+            mb_substr($this->getKey(), 0, 32, '8bit'),
+            OPENSSL_RAW_DATA,
+            $iv
+        );
+
+        // Authentication
+        $hmac = hash_hmac(
+            'SHA256',
+            $iv . $ciphertext,
+            mb_substr($key, 32, null, '8bit'),
+            true
+        );
+        return $hmac . $iv . $ciphertext;
+
+        return base64_encode($data);
+    }
+
+    public function decrypt($data)
+    {
+        $hmac       = mb_substr($data, 0, 32, '8bit');
+        $iv         = mb_substr($data, 32, 16, '8bit');
+        $ciphertext = mb_substr($data, 48, null, '8bit');
+
+        // Authentication
+        $hmacNew = hash_hmac(
+            'SHA256',
+            $iv . $ciphertext,
+            mb_substr($this->getKey(), 32, null, '8bit'),
+            true
+        );
+        if (! $this->hashEquals($hmac, $hmacNew)) {
+            throw new \RuntimeException('Authentication failed');
+        }
+
+        // Decrypt
+        return openssl_decrypt(
+            $ciphertext,
+            'AES-256-CBC',
+            mb_substr($this->getKey(), 0, 32, '8bit'),
+            OPENSSL_RAW_DATA,
+            $iv
+        );
+
         return base64_decode($crypted);
+    }
+
+    public function hashEquals($expected, $actual)
+    {
+        $expected     = (string) $expected;
+        $actual       = (string) $actual;
+        if (function_exists('hash_equals')) {
+            return hash_equals($expected, $actual);
+        }
+        $lenExpected  = mb_strlen($expected, '8bit');
+        $lenActual    = mb_strlen($actual, '8bit');
+        $len          = min($lenExpected, $lenActual);
+        $result = 0;
+        for ($i = 0; $i < $len; $i++) {
+            $result |= ord($expected[$i]) ^ ord($actual[$i]);
+        }
+        $result |= $lenExpected ^ $lenActual;
+        return ($result === 0);
     }
 }
--- src/TweeServerLessSession/Session/Factory/CrypterFactory.php
+++ src/TweeServerLessSession/Session/Factory/CrypterFactory.php
@@ -3,11 +3,16 @@ namespace TweeServerLessSession\Session\Factory;
 
 use TweeServerLessSession\Session\Crypter;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use DomainException;
 
 class CrypterFactory
 {
     public function __invoke(ServiceLocatorInterface $container)
     {
-        return new Crypter();
+        $config = $container->get('config')['di']['instance']['TweeServerLessSession\Session\Factory'];
+        if (empty($config['parameters']['key'])) {
+            throw new DomainException('Secret key not found for [di][instance][TweeServerLessSession\Session\Factory][parameters][key]');
+        }
+        return new Crypter($config['parameters']['key']);
     }
 }
